e2227f4026acd789ef0fa56f07de146407e61a74
GeraltR <donchichot@sielata.com.pl>
2024-06-12 22:54:34 +0200

    Correcting stylings and referenses

--- src/components/CheckboxLink.jsx
+++ src/components/CheckboxLink.jsx
@@ -4,12 +4,19 @@ const CheckboxLink = ({ ...props }) => {
       <div className="inline-flex">
         <label
           className="relative flex items-center p-3 -mt-2 rounded-full cursor-pointer"
-          htmlFor="textLabel"
+          htmlFor={props.name}
         >
           <input
+            disabled={props.disabled}
             type="checkbox"
-            className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
-            id="textLabel"
+            className={`"before:content[''] peer relative h-5 w-5 appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+              ${
+                !props.disabled
+                  ? "cursor-pointer"
+                  : "cursor-not:allowed opacity-10"
+              }
+              `}
+            id={props.name}
             value={props.checked}
             checked={props.checked}
             onChange={props.onChange}
@@ -32,12 +39,22 @@ const CheckboxLink = ({ ...props }) => {
           </span>
         </label>
         <label
-          className="mt-px font-light text-gray-700 cursor-pointer select-none"
-          htmlFor="textLabel"
+          className={`"mt-px font-light text-gray-700 select-none"
+            ${
+              !props.disabled
+                ? "cursor-pointer"
+                : "cursor-not-allowed opacity-10"
+            }
+            `}
+          htmlFor={props.name}
         >
-          <p className="flex font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+          <p
+            disabled={props.disabled}
+            className="flex font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900"
+          >
             {props.description}
             <a
+              disabled={props.disabled}
               href={props.linkAddress}
               className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-500 transition-colors hover:text-blue-700"
             >
