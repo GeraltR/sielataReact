965f515c36184ff8e2f14ffb55ff2b4bf6c31010
GeraltR <donchichot@sielata.com.pl>
2024-06-22 19:26:21 +0200

    Deleting registered pupill

--- src/layouts/PupillsLayot.jsx
+++ src/layouts/PupillsLayot.jsx
@@ -4,18 +4,40 @@ import { RegisterPupillDialog } from "../components/dialogs/RegisterPupillDialog
 import Backdrop from "@mui/material/Backdrop";
 import axios from "../api/axios";
 import ModalSpinner from "../components/main/ModalSpinner";
+import ConfirmationDialog from "../components/dialogs/ConfirmationDialog";
+import useAuthContext from "../context/AuthContext";
 
 export const PupillsLayouts = (props) => {
-  const [open, setOpen] = useState(false);
+  const { csrf } = useAuthContext();
+  const [openRegisterDialog, setOpenRegisterDialog] = useState(false);
+  const [openConfirmationDialog, setOpenConfirmationDialog] = useState({
+    pupill: [],
+    opening: false,
+  });
   const [loading, setLoadaing] = useState(false);
   const [values, setValues] = useState({
     pupills: [],
     loading: true,
   });
 
+  const handleDisagreeConfirmationDialog = () => {
+    setOpenConfirmationDialog({ pupill: [], opening: false });
+    console.log("Disagree");
+  };
+
+  const handleAgreeConfirmationDialog = async () => {
+    const pupillId = openConfirmationDialog.pupill.id;
+    setOpenConfirmationDialog({ pupill: [], opening: false });
+    setLoadaing(true);
+    await csrf();
+    await axios.delete("/api/" + pupillId);
+    getPupills();
+  };
+
   const getPupills = async () => {
     if (props.idopiekuna) {
       setLoadaing(true);
+      await csrf();
       const { data } = await axios.get("/api/pupills/" + props.idopiekuna);
       if (data.status === 200)
         setValues({ pupills: data.pupills, loading: false });
@@ -27,19 +49,15 @@ export const PupillsLayouts = (props) => {
     getPupills();
   }, []);
 
-  const handleOpen = () => {
-    setOpen(true);
+  const handleOpenRegisterDialog = () => {
+    setOpenRegisterDialog(true);
   };
   const handleClose = () => {
-    setOpen(false);
-    getPupills();
+    setOpenRegisterDialog(false);
   };
 
-  const handleDelete = async (pupill) => {
-    console.log(pupill);
-    setLoadaing(true);
-    await axios.delete("/api/" + pupill);
-    getPupills();
+  const handleDelete = (pupill) => {
+    setOpenConfirmationDialog({ pupill: pupill, opening: true });
   };
 
   return (
@@ -50,7 +68,7 @@ export const PupillsLayouts = (props) => {
           <button
             data-modal-target="default-modal"
             className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-            onClick={handleOpen}
+            onClick={handleOpenRegisterDialog}
           >
             Dodaj ucznia
           </button>
@@ -80,13 +98,13 @@ export const PupillsLayouts = (props) => {
                   </div>
                   <div className="xl:flex col-span-1 justify-end mr-2 pt-4">
                     <button
-                      onClick={() => handleDelete(pupill.id)}
+                      onClick={() => handleDelete(pupill)}
                       className="max-w-36 flex justify-end xl:mt-auto ml-2 xl:ml-0 mr-2 xl:mr-1 md:mr-auto mb-2 xl:mb-0 bg-gray-100 text-gray-800 hover:bg-gray-200 font-semibold py-2 px-4 border border-gray-600 rounded shadow"
                     >
                       Zmień
                     </button>
                     <button
-                      onClick={() => handleDelete(pupill.id)}
+                      onClick={() => handleDelete(pupill)}
                       className="max-w-36 flex justify-end xl:mt-auto ml-2 xl:ml-0 mr-2 xl:mr-1 md:mr-auto mb-2 xl:mb-0 bg-red-400 text-gray-800 hover:bg-red-600 hover:text-gray-50 font-semibold py-2 px-4 border border-red-600 rounded shadow"
                     >
                       Usuń ucznia
@@ -102,14 +120,26 @@ export const PupillsLayouts = (props) => {
       </div>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={open}
+        open={openRegisterDialog || openConfirmationDialog.opening}
       >
         <RegisterPupillDialog
           title={`Dodaj ucznia dla: ` + props.teacher}
           idopiekuna={props.idopiekuna}
-          open={open}
+          open={openRegisterDialog}
+          teacherEmail={props.teacherEmail}
           handleClose={handleClose}
+          getPupills={getPupills}
         ></RegisterPupillDialog>
+        <ConfirmationDialog
+          title={"Usuwanie ucznia"}
+          description={`Czy chesz usunąć ucznia: `}
+          pupill={`${openConfirmationDialog.pupill.imie} ${openConfirmationDialog.pupill.nazwisko}`}
+          open={openConfirmationDialog.opening}
+          handleDisagree={handleDisagreeConfirmationDialog}
+          handleAgree={handleAgreeConfirmationDialog}
+          buttonCancel="Anulij"
+          buttonOK="Usuń"
+        ></ConfirmationDialog>
       </Backdrop>
     </>
   );
