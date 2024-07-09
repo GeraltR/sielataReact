d8dfb6e26cb070f8d09b69874674756d3aaf6890
GeraltR <donchichot@sielata.com.pl>
2024-07-09 18:20:52 +0200

    Adding language attribute

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,30 @@ import RegisteredModels from "./pages/RegisteredModels";
 import Jury from "./pages/Jury";
 import ListResults from "./pages/ListResults";
 import Prixes from "./pages/Prixes";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="min-h-screen bg-cover bg-no-repeat bg-[url('./assets/images/bg.jpg')]">
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/listresults" element={<ListResults />} />
-          <Route path="/registeredmodels" element={<RegisteredModels />} />
-          <Route path="/prixies" element={<Prixes />} />
-          <Route path="/jury" element={<Jury />} />
-        </Route>
-        <Route element={<GuestLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/password-reset/:token" element={<ResetPassword />} />
-        </Route>
-      </Routes>
-    </div>
+    <>
+      <Helmet htmlAttributes={{ lang: "pl" }} />
+      <div className="min-h-screen bg-cover bg-no-repeat bg-[url('./assets/images/bg.jpg')]">
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/listresults" element={<ListResults />} />
+            <Route path="/registeredmodels" element={<RegisteredModels />} />
+            <Route path="/prixies" element={<Prixes />} />
+            <Route path="/jury" element={<Jury />} />
+          </Route>
+          <Route element={<GuestLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/password-reset/:token" element={<ResetPassword />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
