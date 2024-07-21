a00cb0c63c47519157ac7178c35da53b532b9569
GeraltR <donchichot@sielata.com.pl>
2024-07-21 19:20:12 +0200

    Starting pages prixes

--- src/pages/Prixes.jsx
+++ src/pages/Prixes.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Prixes() {
-  return <div>Prixes</div>;
+  return (
+    <section className="block xl:grid xl:col-span-2 md:grid md:col-span-1 gap-8 p-1 h-max">
+      <div className="xl:flex md:grid w-[100%] xl:w-[100%] md:w-[100%] bg-white bg-opacity-30 rounded-lg shadow-md shadow-gray-200">
+        <div className="grid divide-y xl:m-5 md:m-5 sm:m-0 justify-items-center">
+          <h3 className="text-2xl font-medium text-gray-800">
+            Łączenie kategorii
+          </h3>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Prixes;
