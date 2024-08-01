52ccc0cfa3a8debcb3b683332affb5fd65540aab
GeraltR <donchichot@sielata.com.pl>
2024-08-01 22:46:59 +0200

    Changing component to tailwind controls

--- src/pages/GrandPrixes.jsx
+++ src/pages/GrandPrixes.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-import { FaSearch } from "react-icons/fa";
 import ModalSpinner from "../components/main/ModalSpinner";
 import axios from "../api/axios";
 
 function GrandPrixes() {
   const [loading, setLoading] = useState(false);
   const [prixes, setPrixes] = useState([]);
-  const [newprix, setNewPrix] = useState("");
 
   const get_prixes = async () => {
     setLoading(true);
@@ -41,7 +39,7 @@ function GrandPrixes() {
             <div className="max-w-2xl justify-items-center p-5">
               <span className="text-lg font-bold">Wybierz nagrodę:</span>
               <select
-                className="bg-gray-50 mb-4 py-3 p-5 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                className="bg-gray-50 mb-4 py-3 p-5 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 value={prixes.id}
                 onChange={handleOnChange}
               >
@@ -51,25 +49,37 @@ function GrandPrixes() {
                   </option>
                 ))}
               </select>
-              <div className="w-[100%] flex">
-                <FaSearch
-                  id="findmodel"
-                  size="3em"
-                  color="blue"
-                  className="bg-gray-50 h-11 px-2"
-                />
+              <label
+                htmlFor="search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only"
+              >
+                szukaj wg numeru lub nazwy
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
                 <input
-                  type="text"
-                  value={newprix}
-                  placeholder="wpisz numer lub nazwę"
-                  className="w-full bg-gray-50 border-x-0 border-indigo-800 h-11 text-left text-gray-900 text-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-1 px-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="search"
+                  id="search"
+                  className="block w-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="szukaj wg numeru lub nazwy"
                 />
-                <button
-                  className="w-[15rem] bg-indigo-500 hover:bg-indigo-300 text-zinc-50 font-semibold py-2 px-4 border border-indigo-500 hover:border-indigo-300 hover:text-zinc-200 rounded shadow"
-                  onClick={handleAddPrix}
-                >
-                  Dodaj nagrodę
-                </button>
+                <button className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 before:content-['+']  md:before:content-['Dodaj\00a0nagrodę'] xl:before:content-['Dodaj\00a0nagrodę']"></button>
               </div>
             </div>
           </div>
