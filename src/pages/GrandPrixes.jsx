e55cc657b53ce01a67a7f6702c47c886185cf1b4
GeraltR <donchichot@sielata.com.pl>
2024-08-01 22:25:29 +0200

    search controlls

--- src/pages/GrandPrixes.jsx
+++ src/pages/GrandPrixes.jsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import ModalSpinner from "../components/main/ModalSpinner";
+import axios from "../api/axios";
 
 function GrandPrixes() {
   const [loading, setLoading] = useState(false);
+  const [prixes, setPrixes] = useState([]);
+  const [newprix, setNewPrix] = useState("");
+
+  const get_prixes = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(`/api/listgrandprixes/0`);
+      setPrixes(data.prixes);
+    } catch (error) {
+      console.log("Error geting prixes");
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    get_prixes();
+  }, []);
+
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleAddPrix = () => {};
 
   return (
     <>
@@ -15,6 +40,37 @@ function GrandPrixes() {
             </h3>
             <div className="max-w-2xl justify-items-center p-5">
               <span className="text-lg font-bold">Wybierz nagrodę:</span>
+              <select
+                className="bg-gray-50 mb-4 py-3 p-5 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                value={prixes.id}
+                onChange={handleOnChange}
+              >
+                {prixes.map((prix) => (
+                  <option key={prix.id} value={prix.id}>
+                    {prix.prix_name}
+                  </option>
+                ))}
+              </select>
+              <div className="w-[100%] flex">
+                <FaSearch
+                  id="findmodel"
+                  size="3em"
+                  color="blue"
+                  className="bg-gray-50 h-11 px-2"
+                />
+                <input
+                  type="text"
+                  value={newprix}
+                  placeholder="wpisz numer lub nazwę"
+                  className="w-full bg-gray-50 border-x-0 border-indigo-800 h-11 text-left text-gray-900 text-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-1 px-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                <button
+                  className="w-[15rem] bg-indigo-500 hover:bg-indigo-300 text-zinc-50 font-semibold py-2 px-4 border border-indigo-500 hover:border-indigo-300 hover:text-zinc-200 rounded shadow"
+                  onClick={handleAddPrix}
+                >
+                  Dodaj nagrodę
+                </button>
+              </div>
             </div>
           </div>
         </div>
