07bfda5b5b1840b7dba74135d05a8cff8b48856f
GeraltR <donchichot@sielata.com.pl>
2025-10-05 16:32:31 +0200

    Reducet bundle size

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,57 @@
 import { Route, Routes } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { Suspense, lazy } from "react";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import AuthLayout from "./layouts/AuthLayout";
-import GuestLayout from "./layouts/GuestLayout";
-import RegisteredModels from "./pages/RegisteredModels";
-import Jury from "./pages/Jury";
-import ListResults from "./pages/ListResults";
-import ConnecCategories from "./pages/ConnecCategories";
-import ModelCard from "./pages/print/ModelCard";
-import Qualification from "./pages/Qualification";
-import GrandPrixes from "./pages/GrandPrixes";
-import Parameters from "./pages/Parameters";
-import DiplomasList from "./pages/DiplomasList";
-import ResultsCompetition from "./pages/ResultsCompetition";
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword"));
+const AuthLayout = lazy(() => import("./layouts/AuthLayout"));
+const GuestLayout = lazy(() => import("./layouts/GuestLayout"));
+const Jury = lazy(() => import("./pages/Jury"));
+const RegisteredModels = lazy(() => import("./pages/RegisteredModels"));
+const ListResults = lazy(() => import("./pages/ListResults"));
+const ConnecCategories = lazy(() => import("./pages/ConnecCategories"));
+const ModelCard = lazy(() => import("./pages/print/ModelCard"));
+const Qualification = lazy(() => import("./pages/Qualification"));
+const GrandPrixes = lazy(() => import("./pages/GrandPrixes"));
+const DiplomasList = lazy(() => import("./pages/DiplomasList"));
+const ResultsCompetition = lazy(() => import("./pages/ResultsCompetition"));
+const Parameters = lazy(() => import("./pages/Parameters"));
 
 function App() {
   return (
     <>
       <Helmet htmlAttributes={{ lang: "pl" }} />
       <div className="min-h-screen bg-cover bg-no-repeat bg-[url('./assets/images/bg.jpg')] print:bg-none">
-        <Routes>
-          <Route element={<AuthLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/listresults" element={<ListResults />} />
-            <Route path="/registeredmodels" element={<RegisteredModels />} />
-            <Route path="/conneccategories" element={<ConnecCategories />} />
-            <Route path="/qualification" element={<Qualification />} />
-            <Route path="/grandprixes" element={<GrandPrixes />} />
-            <Route path="/diplomaslist" element={<DiplomasList />} />
-            <Route path="/jury" element={<Jury />} />
-            <Route path="/parameters" element={<Parameters />} />
-            <Route path="/printmodelcard" element={<ModelCard />} />
-            <Route
-              path="/competitionresults"
-              element={<ResultsCompetition />}
-            />
-          </Route>
-          <Route element={<GuestLayout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/password-reset/:token" element={<ResetPassword />} />
-            <Route path="/results" element={<ResultsCompetition />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div>Ładowanie...</div>}>
+          <Routes>
+            <Route element={<AuthLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/listresults" element={<ListResults />} />
+              <Route path="/registeredmodels" element={<RegisteredModels />} />
+              <Route path="/conneccategories" element={<ConnecCategories />} />
+              <Route path="/qualification" element={<Qualification />} />
+              <Route path="/grandprixes" element={<GrandPrixes />} />
+              <Route path="/diplomaslist" element={<DiplomasList />} />
+              <Route path="/jury" element={<Jury />} />
+              <Route path="/parameters" element={<Parameters />} />
+              <Route path="/printmodelcard" element={<ModelCard />} />
+              <Route
+                path="/competitionresults"
+                element={<ResultsCompetition />}
+              />
+            </Route>
+            <Route element={<GuestLayout />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/password-reset/:token" element={<ResetPassword />} />
+              <Route path="/results" element={<ResultsCompetition />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </div>
     </>
   );
--- src/pages/Parameters.jsx
+++ src/pages/Parameters.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Parameters() {
   return <div>Parameters</div>;
 }
