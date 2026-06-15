7dd76e3000326582eb0e0000aeb2461a542d1da0
GeraltR <donchichot@sielata.com.pl>
2026-06-15 18:26:08 +0200

    Fixing the drop-down menu

--- src/components/main/Dropdown.jsx
+++ src/components/main/Dropdown.jsx
@@ -7,7 +7,7 @@ const Dropdown = ({ submenus, dropdown, depthLevel }) => {
     <ul
       className={`z-10 ${
         dropdown ? "block" : "hidden"
-      } absolute md:fixed item-center flex-col bg-neutral-500 px-4 leading-7 rounded-lg shadow-2xl`}
+      } absolute item-center flex-col bg-neutral-500 px-4 leading-7 rounded-lg shadow-2xl`}
     >
       {submenus.map((submenu, index) =>
         submenu.type === "separator" ? (
